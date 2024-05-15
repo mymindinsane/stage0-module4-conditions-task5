c41aa488c4872d9bdffff22208a297eec1d31cc0
mymindinsane <arturguletskiy21@gmail.com>
2024-05-15 19:56:24 +0300

    done LeapYearPrinter.java

--- src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
@@ -2,5 +2,11 @@ package school.mjc.stage0.conditions.task5;
 
 public class LeapYearPrinter {
     public void isLeapYear(int year) {
+        if (year == 366){
+            System.out.println("leap");
+        }
+        else {
+            System.out.println("not leap");
+        }
     }
 }
